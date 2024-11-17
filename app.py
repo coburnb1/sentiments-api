6ab64f5acf66aef8e18d2fa47d9168c4e4ddcd32
coburnb1 <coburnb1@nku.edu>
2024-11-16 19:43:19 -0500

    Updating procfile and app.py to work with gunicorn

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-# app.py
-
 from flask import Flask
 from api import api_bp
 
 def create_app():
     app = Flask(__name__)
+    # Register blueprints
     app.register_blueprint(api_bp, url_prefix='/api')
     return app
 
+# Create the application instance for Gunicorn
+app = create_app()
+
+# Optional: Run the development server locally
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
